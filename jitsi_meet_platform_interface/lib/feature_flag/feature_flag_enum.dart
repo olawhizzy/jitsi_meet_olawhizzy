62bd31efb564b06081716b0fca502bd012a7c1a9
Abdulazeez Ismail <aismail@creativeadvtech.com>
2022-09-29 13:31:03 +0100

    add adroid share

--- jitsi_meet_platform_interface/lib/feature_flag/feature_flag_enum.dart
+++ jitsi_meet_platform_interface/lib/feature_flag/feature_flag_enum.dart
@@ -1,5 +1,5 @@
 /// Enumeration of all available feature flags
-/// Reflects the official list of Jitsi Meet SDK 2.9.0 feature flags
+/// Reflects the official list of Jitsi Meet SDK 2.11.0 feature flags
 /// https://github.com/jitsi/jitsi-meet/blob/master/react/features/base/flags/constants.js
 enum FeatureFlagEnum {
   /// Flag indicating if add-people functionality should be enabled.
@@ -10,8 +10,7 @@ enum FeatureFlagEnum {
   /// Default: enabled (true) on Android, auto-detected on iOS.
   CALENDAR_ENABLED,
 
-  /// Flag indicating if call integration (CallKit on iOS, 
-  /// ConnectionService on Android)
+  /// Flag indicating if call integration (CallKit on iOS, ConnectionService on Android)
   /// should be enabled.
   /// Default: enabled (true).
   CALL_INTEGRATION_ENABLED,
@@ -19,6 +18,10 @@ enum FeatureFlagEnum {
   /// Flag indicating if close captions should be enabled.
   /// Default: enabled (true).
   CLOSE_CAPTIONS_ENABLED,
+
+  /// Flag indicating if conference timer should be enabled.
+  /// Default: enabled (true).
+  CONFERENCE_TIMER_ENABLED,
 
   /// Flag indicating if chat should be enabled.
   /// Default: enabled (true).
@@ -32,6 +35,10 @@ enum FeatureFlagEnum {
   /// Default: disabled (false).
   IOS_RECORDING_ENABLED,
 
+  /// Flag indicating if kickout is enabled.
+  /// Default: enabled (true).
+  KICK_OUT_ENABLED,
+
   /// Flag indicating if live-streaming should be enabled.
   /// Default: auto-detected.
   LIVE_STREAMING_ENABLED,
@@ -40,9 +47,9 @@ enum FeatureFlagEnum {
   /// Default: enabled (true).
   MEETING_NAME_ENABLED,
 
-  /// Flag indicating if the meeting password button should be enabled. Note 
-  /// that this flag just decides on the buttton, if a meeting has a password
-  /// set, the password ddialog will still show up.
+  /// Flag indicating if the meeting password button should be enabled.
+  /// Note that this flag just decides on the button, if a meeting has a password
+  /// set, the password dialog will still show up.
   /// Default: enabled (true).
   MEETING_PASSWORD_ENABLED,
 
@@ -58,6 +65,15 @@ enum FeatureFlagEnum {
   /// Default: auto-detected.
   RECORDING_ENABLED,
 
+  /// Flag indicating the local and (maximum) remote video resolution. Overrides
+  /// the server configuration.
+  /// Default: (unset).
+  RESOLUTION,
+
+  /// Flag indicating if server URL change is enabled.
+  /// Default: enabled (true)
+  SERVER_URL_CHANGE_ENABLED,
+
   /// Flag indicating if tile view feature should be enabled.
   /// Default: enabled.
   TILE_VIEW_ENABLED,
@@ -66,7 +82,15 @@ enum FeatureFlagEnum {
   /// Default: disabled (false).
   TOOLBOX_ALWAYS_VISIBLE,
 
+  /// Flag indicating if the video share button should be enabled
+  /// Default: enabled (true).
+  VIDEO_SHARE_BUTTON_ENABLED,
+
   /// Flag indicating if the welcome page should be enabled.
   /// Default: disabled (false).
   WELCOME_PAGE_ENABLED,
+
+  /// Flag indicating if the screensharing should be enabled.
+  /// Default: disabled (false).
+  ANDROID_SCREENSHARING_ENABLED,
 }
